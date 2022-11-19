5b4622e30481b7b04b2f43bd925333ba37470aac
Christian Seidlitz <seidchr+github@gmail.com>
2022-11-19 18:17:54 +0100

    allow more output options for tests

--- Invoke-Tests.ps1
+++ Invoke-Tests.ps1
@@ -1,1 +1,11 @@
-Invoke-Pester -Output Detailed $PSScriptRoot
+#Requires -Modules Pester -PSEdition Core
+
+param(
+    [ValidateSet('Detailed', 'Diagnostic', 'Minimal', 'None', 'Normal')]
+    $Output = 'Detailed'
+)
+
+Invoke-Pester -Output:$Output $PSScriptRoot
+
+# Writing Tests for and with Modules
+# https://pester.dev/docs/usage/modules
