5e4cbc3f1ecbbea8f241c649e45c4eb849e206ff
Christian Seidlitz <seidchr+github@gmail.com>
2022-11-22 19:36:32 +0100

    to query improvements

--- Tests/ToQuery.Tests.ps1
+++ Tests/ToQuery.Tests.ps1
@@ -2,23 +2,18 @@ BeforeAll {
     Import-Module .\PoshToDon -Force
 }
 
-Describe 'ToQuery' {
-    It 'should handle normal entries' {
-        InModuleScope PoshToDon {
+InModuleScope PoshToDon {
+    Describe 'ToQuery' {
+        It 'should handle normal entries' {
             @{ entry = "test" } | ToQuery | Should -Be "?entry=test"
         }
-    }
-    It 'should handle array entries' {
-        InModuleScope PoshToDon {
+        It 'should handle array entries' {
             @{ entry = @( 1, 2 ) } | ToQuery | Should -Be "?entry[]=1&entry[]=2"
         }
-    }
-    It 'should handle existing parameters' {
-        InModuleScope PoshToDon {
+        It 'should handle existing parameters' {
             $query = @{ entry = "test" } | AddData @{ other = "test2" } | ToQuery 
             $query | Should -BeLike "?*entry=test*"
             $query | Should -BeLike "?*other=test2*"
         }
     }
 }
-
